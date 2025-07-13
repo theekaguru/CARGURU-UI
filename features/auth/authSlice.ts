3eb6bb167407cbeea9a9892905fbb1374adf0586
theekaguru <danielkaguru2001@gmail.com>
2025-07-13 11:48:21 +0300

    authslice to  manage user login data added

--- features/auth/authSlice.ts
+++ features/auth/authSlice.ts
@@ -1,14 +1,29 @@
 import {createSlice , type PayloadAction} from "@reduxjs/toolkit"
 
 interface AuthState{
-    user: null ;
-    token: string
+    user: null | any;
+    token: string | null ;
+    isAuthenticated: boolean;
+    userType: string | null
 }
 
-const initialState ={
+const initialState:AuthState ={
     user: null , 
     token: null ,
     isAuthenticated: false ,
     userType: null 
 
 }
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState, 
+    reducers:{
+        setCredentials:(state , action:PayloadAction<{user:any , token:string , userType:any}>)=>{
+          state.user = action.payload.user;
+          state.token = action.payload.token;
+          state.userType = action.payload.userType;
+          state.isAuthenticated = true
+        }
+    }
+})
